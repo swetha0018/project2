4101c81f342e2fab2e3edf8288d84a82eb84035a
swetha0018 <71234267+swetha0018@users.noreply.github.com>
2020-10-31 14:53:13 -0500

    Update if_else.sh

--- if_else.sh
+++ if_else.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# The purpose of this block is to demonstrate a basic understanding of the if-ekse command.
+
 num_a=400
 num_b=200
 
+# this part of the code is very important because the brackets are used to confine the major part of the equation to the contents of the brackets.
+# Without the brackets, the operation could produce an error or it might not calculate the way you want it to. Great job!
 if [ $num_a -lt $num_b ]; then
 	echo "$num_a is less than $num_b!"
 
