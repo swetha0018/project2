03bd78ef3b61cdf95a09d51beaa87c13c3418231
swetha0018 <71234267+swetha0018@users.noreply.github.com>
2020-10-31 15:03:32 -0500

    Update until.sh

--- until.sh
+++ until.sh
@@ -1,6 +1,11 @@
 #!/bin/bach
 
+# The purpose of the Until command is to execute a certain function until another condition is met. This block demonstrates a basic use of this command.
+
 counter=4
+
+# In my opinion the until command is similar to a "do-while" loop, except it is in reverse. The function shows the loop controlling variable first and it then shows... 
+# the function that is to be executed. Well written code, great job!
 until [ $counter -lt 3 ]; do
 	let counter-=1
 	echo $counter 
